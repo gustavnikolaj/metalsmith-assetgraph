f23a10236d16fb4b412388b5d1d084c02c427ae7
Gustav Nikolaj Olsen <gno@one.com>
2014-10-23 22:30:54 +0000

    add testcase that covers a very basic build

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,20 +1,65 @@
 var expect = require('unexpected');
 var assetGraph = require('../lib/index');
+var Metalsmith = require('metalsmith');
+var Path = require('path');
+var rimraf = require('rimraf');
+var glob = require('glob');
+var fs = require('fs');
+var async = require('async');
 
 describe('metalsmith-assetgraph', function () {
     describe('basic', function () {
         it('should export a function', function () {
             expect(assetGraph, 'to be a function');
         });
-        it('should throw an exception if required options is not provided', function () {
-            expect(function () {
-                assetGraph();
-            }, 'to throw', 'metalsmith-assetgraph: you must provide the root of the source folder in options.root');
-        });
         it('should return a function of arity 3', function () {
             var middleware = assetGraph({root: 'src'});
             expect(middleware, 'to be a function');
             expect(middleware, 'to have length', 3);
+        });
+    });
+    describe('test cases', function () {
+        function deleteBuildFolder(done) {
+            rimraf(Path.resolve(__dirname, 'build'), done);
+        }
+        before(deleteBuildFolder);
+        afterEach(deleteBuildFolder);
+        function readTestData(path, callback) {
+            glob(path + '/**.*', function (err, files) {
+                if (err) {
+                    return callback(err, null);
+                }
+                var result = {};
+                async.each(files, function (file, done) {
+                    fs.readFile(file, 'utf-8', function (err, data) {
+                        result[file.replace(path, '')] = data;
+                        done(err);
+                    });
+                }, function (err) {
+                    callback(err, result);
+                });
+            });
+        }
+        [
+            'singlePageWithTwoCssFiles'
+        ].forEach(function (testCase) {
+            it(testCase, function (done) {
+                readTestData(Path.resolve(__dirname, 'fixtures', testCase, 'expected'), function (err, expectedData) {
+                    var m = Metalsmith(__dirname);
+                    m.source('fixtures/' + testCase + '/src')
+                    m.destination('build');
+                    m.use(assetGraph({
+                        version: 'testFixture'
+                    }));
+                    m.build(function (err) {
+                        expect(err, 'to be null');
+                        readTestData(Path.resolve(__dirname, 'build'), function (err, actualData) {
+                            expect(expectedData, 'to equal', actualData);
+                            done();
+                        });
+                    });
+                });
+            });
         });
     });
 });
